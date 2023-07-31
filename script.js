01f8090e59fcc985f8fb923eabff7348d1b783d6
Slawomir Was <was.slawek@gmail.com>
2023-07-31 13:01:24 +0200

    stbx-copy

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+	document.querySelector(".company").src = anything;
+}
+
+function changeCircleColor(color) {
+	const circle = document.querySelector(".circle");
+	circle.style.background = color;
+}
